d969273caddf1c7ee2ee0550b40bdc2c34335195
alfonsspj <alfonsspj@gmail.com>
2024-05-23 17:12:25 -0300

    Generar jwten el servicio

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { config } = require('./../config/config');
 
 
 const router = express.Router();
 
 router.post('/login',
-    passport.authenticate('local', { session: false }),
+    passport.authenticate('local', { session: false }),  // capa de autenticación
     async (req, res, next) => {
         try {
-            res.json(req.user);
+            const user = req.user;
+            // firma del token
+            const payload = {
+                sub: user.id,
+                role: user.role
+            };
+            const token = jwt.sign(payload, config.jwtSecret);
+            res.json({
+                user,
+                token
+            });
         } catch (error) {
             next(error);
         }
